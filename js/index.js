b0fa9159d6b12055d1e52b6244990d5b253459ad
XIE Hui <hui.x@outlook.com>
2018-03-17 10:53:51 +0800

    v0.3

--- js/index.js
+++ js/index.js
@@ -27,53 +27,57 @@ function validateForm() {
   return flag;
 }
 
-var img0 = new Image();
-img0.src = "./images/1.jpg";
-img0.load = function () {
-  document.getElementById("index-banner").style.backgroundImage = 'url("./images/1.jpg")';
-};
-var img1 = new Image();
-img1.src = "./images/2.jpg";
-img1.load = function () {
-  document.getElementById("index-banner").style.backgroundImage = 'url("./images/2.jpg")';
-};
-var img2 = new Image();
-img2.src = "./images/3.jpg";
-img2.load = function () {
-  document.getElementById("index-banner").style.backgroundImage = 'url("./images/3.jpg")';
-};
-var img3 = new Image();
-img3.src = "./images/4.jpg";
-img3.load = function () {
-  document.getElementById("index-banner").style.backgroundImage = 'url("./images/4.jpg")';
-};
-
-var pic_arr = [img0, img1, img2, img3];
-var index = 1;
-
 window.onload = function () {
+  if (this.document.documentElement.clientWidth >= 1400) {
+    document.getElementById("login-container").style.height = "720px";
+    document.getElementById("index-banner").style.height = "720px";
+  }
+  var index_banner = document.getElementById("index-banner");
+  var img0 = new Image();
+  img0.src = "./images/1.jpg";
+  img0.load = function () {
+    index_banner.style.backgroundImage = 'url("./images/1.jpg")';
+  };
+  var img1 = new Image();
+  img1.src = "./images/2.jpg";
+  img1.load = function () {
+    index_banner.style.backgroundImage = 'url("./images/2.jpg")';
+  };
+  var img2 = new Image();
+  img2.src = "./images/3.jpg";
+  img2.load = function () {
+    index_banner.style.backgroundImage = 'url("./images/3.jpg")';
+  };
+  var img3 = new Image();
+  img3.src = "./images/4.jpg";
+  img3.load = function () {
+    index_banner.style.backgroundImage = 'url("./images/4.jpg")';
+  };
+  var pic_arr = [img0, img1, img2, img3];
+  var index = 1;
+  var dots = document.getElementsByClassName("focus-anchor");
   pic_arr[0].load();
-  document.getElementsByClassName("focus-anchor")[0].style.opacity = "1";
-  for (var i=0; i<4; i++) {
-    document.getElementsByClassName("focus-anchor")[i].onmouseup = function(event) {
+  dots[0].style.opacity = "1";
+  for (var i = 0; i < 4; i++) {
+    dots[i].onmouseup = function (event) {
       event = event ? event : window.event;
       var obj = event.srcElement ? event.srcElement : event.target;
       index = obj.id;
       pic_arr[index].load();
-      document.getElementsByClassName("focus-anchor")[index].style.opacity = "1";
-      for (var i=0; i<4; i++) {
+      dots[index].style.opacity = "1";
+      for (var i = 0; i < 4; i++) {
         if (i != index) {
-          document.getElementsByClassName("focus-anchor")[i].style.opacity = ".5";
+          dots[i].style.opacity = ".5";
         }
       }
     };
   }
   setInterval(function () {
     pic_arr[index].load();
-    document.getElementsByClassName("focus-anchor")[index].style.opacity = "1";
-    for (var i=0; i<4; i++) {
+    dots[index].style.opacity = "1";
+    for (var i = 0; i < 4; i++) {
       if (i != index) {
-        document.getElementsByClassName("focus-anchor")[i].style.opacity = ".5";
+        dots[i].style.opacity = ".5";
       }
     }
     if (index < 3) {
@@ -82,4 +86,14 @@ window.onload = function () {
       index = 0;
     }
   }, 2000);
+};
+
+window.onresize = function () {
+  if (this.document.documentElement.clientWidth >= 1400) {
+    document.getElementById("login-container").style.height = "720px";
+    document.getElementById("index-banner").style.height = "720px";
+  } else {
+    document.getElementById("login-container").style.height = "550px";
+    document.getElementById("index-banner").style.height = "550px";
+  }
 };

5015ab54164ee10ad1bf8dec16988f264ce27b60
XIE Hui <hui.x@outlook.com>
2018-03-18 19:44:33 +0800

    delete //

--- js/index.js
+++ js/index.js
@@ -5,10 +5,7 @@ function validateForm() {
   var at_pos = email_address.indexOf("@");
   var dot_pos = email_address.lastIndexOf(".");
   if (at_pos < 1 || dot_pos < at_pos + 2 || dot_pos + 2 >= email_address.length) {
-    //alert("Please input an email address!");
-    //email_warn.innerText = "This isn't an email";
     email_warn.style.display = "block";
-    //return false;
     flag = false;
   } else {
     email_warn.style.display = "none";
@@ -16,10 +13,7 @@ function validateForm() {
   var password = document.forms["login-form"].password.value;
   var pwd_warn = document.getElementById("pwd-warning");
   if (password == null || password === "") {
-    //alert("Please input password!");
-    //pwd_warn.innerText = "can't be empty";
     pwd_warn.style.display = "block";
-    //return false;
     flag = false;
   } else {
     pwd_warn.style.display = "none";
